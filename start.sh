be96c8899012546a94930d45693ad9ca90de57c9
Tom Erickson <tome326@gmail.com>
2025-11-22 19:53:30 -0600

    update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-mkdir -p /tmp/tailscale
-tailscaled \
-    --state=/tmp/tailscale/tailscaled.state \
-    --socket=/tmp/tailscale/tailscaled.sock \
-    --tun=userspace-networking \
-    --socks5-server=localhost:1055 &
+tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 
 sleep 5
 
-tailscale up  \
-    --authkey=${TAILSCALE_AUTHKEY} \
-    --hostname=marketing-api \
-    --accept-dns=false \
-    --accept-routes \
-    --advertise-tags=tag:render
+tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=marketing-api --accept-dns=false
 
-sleep 10
-
-tailscale status
+sleep 5
 
 exec dotnet MarketingAPI.dll
